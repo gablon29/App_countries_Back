909121e6a625e9da7663643950eaf2769efce146
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-05-22 20:51:27 -0300

    nuevo en el archivo/app.js para acceder mediante cualquier ruta

--- api/index.js
+++ api/index.js
@@ -19,10 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { PORT } = process.env
+
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
